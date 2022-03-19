a87ee0950d7a572d0b97cf34e2af492f95063fac
Ammar Botonjić <botonjic.ab@pg.com>
2022-03-19 16:16:15 +0100

    Setting up routes

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use('/api/users', require('./routes/users'))
+
 app.listen(port, () => console.log(`Server is running on port ${port}`))
