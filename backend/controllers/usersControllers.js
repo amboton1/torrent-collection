dad89904ef94f7b4836fff3c3ec48bea157afddb
Ammar Botonjić <botonjic.ab@pg.com>
2022-03-20 17:01:08 +0100

    Creating user model

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,7 +1,10 @@
 const expressAsyncHandler = require("express-async-handler")
+const Users = require("../models/userModel")
 
 const getUsers = expressAsyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get users'})
+    const users = await Users.find();
+
+    res.status(200).json(users)
 })
 
 const createUser = expressAsyncHandler(async (req, res) => {
@@ -9,15 +12,39 @@ const createUser = expressAsyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add a new user')
     }
-    res.status(200).json({message: `Create user with the id of ${res}`})
+
+    const user = Users.create({
+        name: req.body.name,
+        email: req.body.email
+    })
+    
+    res.status(200).json(user)
 })
 
 const updateUser = expressAsyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update user with the id ${req.params.id}`})
+    const user = await Users.findById(req.params.id)
+
+    if (!user) {
+        res.status(400)
+        throw new Error('User not found')
+    }
+
+    const updatedUser = await Users.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(updatedUser)
 })
 
 const deleteUser = expressAsyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete user with the id ${req.params.id}`})
+    const user = await Users.findById(req.params.id)
+
+    if (!user) {
+        res.status(400)
+        throw new Error('User not found')
+    }
+
+    await user.remove()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = { getUsers, createUser, updateUser, deleteUser }
