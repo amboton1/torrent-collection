56e1ddbd096a3c6a26fb0cd2e42d2e6303409947
Ammar Botonjić <botonjic.ab@pg.com>
2022-03-20 16:02:20 +0100

    MongoDB Setup and Connect to a DB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
