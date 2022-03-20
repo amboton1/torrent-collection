3523e4868f591d9ddc3bfc97a29a67ad3dadce8a
Ammar Botonjić <botonjic.ab@pg.com>
2022-03-20 14:43:43 +0100

    Setting up body data and adding express async handler

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,17 +1,23 @@
-const getUsers = (req, res) => {
+const expressAsyncHandler = require("express-async-handler")
+
+const getUsers = expressAsyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get users'})
-}
+})
 
-const createUser = (req, res) => {
-    res.status(200).json({message: 'Create user'})
-}
+const createUser = expressAsyncHandler(async (req, res) => {
+    if (!req.body.name) {
+        res.status(400)
+        throw new Error('Please add a new user')
+    }
+    res.status(200).json({message: `Create user with the id of ${res}`})
+})
 
-const updateUser = (req, res) => {
+const updateUser = expressAsyncHandler(async (req, res) => {
     res.status(200).json({message: `Update user with the id ${req.params.id}`})
-}
+})
 
-const deleteUser = (req, res) => {
+const deleteUser = expressAsyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete user with the id ${req.params.id}`})
-}
+})
 
 module.exports = { getUsers, createUser, updateUser, deleteUser }
