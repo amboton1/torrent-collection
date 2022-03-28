f3f628a68dc129629612bba4724bda66fa477256
Ammar Botonjić <botonjic.ab@pg.com>
2022-03-28 13:27:02 +0200

    Removing custom css, adding tailwind

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,16 +5,16 @@ const Header = () => {
   return (
     <header className="header flex justify-between items-center py-5 px-14 mb-14 border-b border-[#76cc52]">
         <div className="logo">
-            <Link to='/'>Home</Link>
+            <Link className="text-white" to='/'>Home</Link>
         </div>
-        <ul>
-            <li>
-                <Link to='/login'>
+        <ul className="flex justify-between items-center">
+            <li className="ml-5 leading-[2.2]">
+                <Link className="flex items-center text-white hover:text-[#777]" to='/login'>
                     <FaSignInAlt /> Login
                 </Link>
             </li>
-            <li>
-                <Link to='/register'>
+            <li className="ml-5 leading[2.2]">
+                <Link className="flex items-center text-white hover:text-[#777]" to='/register'>
                     <FaUser /> Register
                 </Link>
             </li>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -20,32 +20,33 @@ const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault()
+    console.log(formData)
   }
 
   return <>
-    <section className="heading">
-      <h1>
-        <FaUser /> Register
+    <section className="flex items-center m-0 mx-auto mb-5 py-0 px-5 font-bold w-2/4 flex-col text-xl">
+      <h1 className="flex items-center mb-2">
+        <FaUser className="mr-0.5" /> Register
       </h1>
       <p>Please create an account</p>
     </section>
 
-    <section className="form">
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <input type="text" name="name" id="name" onChange={onChangeHandler} value={name} placeholder="Enter your name" />
+    <section className="md:w-2/5 sm:w-full lg:w-1/5 m-0 mx-auto">
+      <form autoComplete="off" onSubmit={onSubmit}>
+        <div className="mb-2.5 form-group">
+          <input type="text" className="rounded-none border-0 border-b-2 mb-3 border-b-[#35df47] w-full p-2.5 bg-transparent	outline-0" name="name" id="name" onChange={onChangeHandler} value={name} placeholder="Enter your name" />
+        </div>
+        <div className="mb-2.5 form-group">
+          <input type="email" className="rounded-none border-0 border-b-2 mb-3 border-b-[#35df47] w-full p-2.5 bg-transparent	outline-0" name="email" autoComplete="off" id="email" onChange={onChangeHandler} value={email} placeholder="Enter your email" />
+        </div>
+        <div className="mb-2.5 form-group">
+          <input type="password" className="rounded-none border-0 border-b-2 mb-3 border-b-[#35df47] w-full p-2.5 bg-transparent	outline-0" name="password" autoComplete="off" id="password" onChange={onChangeHandler} value={password} placeholder="Enter your password" />
         </div>
         <div className="form-group">
-          <input type="email" name="email" autoComplete="off" id="email" onChange={onChangeHandler} value={email} placeholder="Enter your email" />
+          <input type="password" className="rounded-none border-0 border-b-2 mb-3 border-b-[#35df47] w-full p-2.5 bg-transparent	outline-0" name="confirmedPassword" id="confirmedPassword" onChange={onChangeHandler} value={confirmedPassword} placeholder="Confirm your password" />
         </div>
         <div className="form-group">
-          <input type="password" name="password" autoComplete="off" id="password" onChange={onChangeHandler} value={password} placeholder="Enter your password" />
-        </div>
-        <div className="form-group">
-          <input type="password" name="confirmedPassword" id="confirmedPassword" onChange={onChangeHandler} value={confirmedPassword} placeholder="Confirm your password" />
-        </div>
-        <div className="form-group">
-          <button type="submit" className="btn btn-block">Submit</button>
+          <button type="submit" className="mt-2 flex items-center justify-center bg-[#26134b] py-2.5 px-5 font-bold rounded-md mb-5 w-full hover:scale-[0.98]">Submit</button>
         </div>
       </form>
     </section>
