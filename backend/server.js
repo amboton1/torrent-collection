7c8298ed536a0e7f1c8ca4e9eacec13502e72666
Ammar Botonjić <botonjic.ab@pg.com>
2022-05-29 12:48:11 +0200

    API setup, login and register fix

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 connectDB()
 
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
