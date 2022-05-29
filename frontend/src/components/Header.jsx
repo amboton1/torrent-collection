a9817c369f9c65eedc883911b2d00ee7ff169a8a
Ammar Botonjić <botonjic.ab@pg.com>
2022-05-29 20:07:52 +0200

    Frontend fixes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -22,11 +22,19 @@ const Header = () => {
         <ul className="flex justify-between items-center">
             {
                 loggedState ? (
+                    <>
+                    <form action="" className="relative mr-7 w-max">
+                        <input type="search" className="peer cursor-pointer transition duration-300 relative z-10 h-12 w-12 rounded-full border bg-transparent pl-12 outline-none border-indigo-600 focus:w-full focus:cursor-text focus:border-lime-300 focus:pl-16 focus:pr-4" />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-gray-500 px-3.5 peer-focus:border-lime-300 peer-focus:stroke-lime-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                    </form>
                     <li>
                         <button className="flex items-center justify-between gap-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={onLogout}>
                             <FaSignOutAlt /> <span>Logout</span>
                         </button>
                     </li>
+                    </>
                 ) : (
                     <>
                         <li className="ml-5 leading-[2.2]">
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,12 @@
-const Dashboard = ({movies}) => {
-
+const Dashboard = ({movies, user}) => {
   const renderListOfMovies = (moviesList) => {
-    return moviesList?.movies.map(element => {
+    return moviesList?.movies.map((element, index) => {
       return (
-        <div className="overlay-div transition duration-500 hover:scale-105">
+        <div key={index} className="hover:brightness-50 relative transition cursor-pointer duration-500 hover:scale-105">
+          <div className="opacity-0 hover:opacity-100 duration-500">
+            <button class="mb-20 absolute inset-0 z-10 flex justify-center items-center text-3xl text-white font-semibold">Get Details!</button>
+            <a href="fff" class="absolute inset-0 z-10 flex justify-center items-center text-3xl text-white font-semibold">Download</a>
+          </div>
           <img src={element?.large_cover_image} alt={element.title} />
         </div>
       );
@@ -12,8 +15,8 @@ const Dashboard = ({movies}) => {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold text-white mb-5">Dashboard</h1>
-      <section className="list-of-movies">
+      <h1 className="text-3xl font-bold text-white mb-5">{`Welcome, ${user?.name}`}</h1>
+      <section className="pb-5">
         <div className="grid grid-cols-4 gap-4 px-5">
           {renderListOfMovies(movies)}
         </div>
@@ -22,4 +25,4 @@ const Dashboard = ({movies}) => {
   )
 }
 
-export default Dashboard
+export default Dashboard;
